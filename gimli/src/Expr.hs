8b277205907d7b38c0427cdb2c581998074fdaa1
Tom Moertel <tom@moertel.com>
2005-09-07 20:41:00 +0000

    Reformatted Expr source
    darcs-hash:20050907204131-8fbd9-f2c6ac5e9c6c0a3d6deb2a75c75f02ba1db2f6ce.gz

--- gimli/src/Expr.hs
+++ gimli/src/Expr.hs
@@ -13,30 +13,32 @@ import Value
 import PPrint
 
 data Expr
-  = EVal Value
-  | EBinOp BinOp Expr Expr
-  | EVector [Expr]
-  | EUOp UnaryOp Expr
-  | ESeries [Expr]
-  | EVar Identifier
-  | EBind Expr Expr
-  | ESelect Expr Expr
-  | EProject Expr PSpec
-  | ETable [(Identifier, Expr)]
-  | EReadCsv Expr
-  | EReadWsv Expr
-  | EWriteWsv Expr Expr
-  | EJoin JoinOp Expr Expr
-  deriving (Show, Read, Eq, Ord)
+    = EVal Value
+    | EBinOp !BinOp Expr Expr
+    | EVector [Expr]
+    | EUOp !UnaryOp Expr
+    | ESeries [Expr]
+    | EVar Identifier
+    | EBind Expr Expr
+    | ESelect Expr Expr
+    | EProject Expr PSpec
+    | ETable [(Identifier, Expr)]
+    | EReadCsv Expr
+    | EReadWsv Expr
+    | EWriteWsv Expr Expr
+    | EJoin JoinOp Expr Expr
+    deriving (Show, Read, Eq, Ord)
 
-data BinOp = BinOpTimes | BinOpDiv | BinOpAdd | BinOpSub | BinOpEq | BinOpNeq
-           | BinOpBindL | BinOpBindR | BinOpEllipses
-           | BinOpGt | BinOpGe | BinOpLt | BinOpLe
-           | BinOpSOr | BinOpSAnd | BinOpVOr | BinOpVAnd
-           | BinOpIn
+data BinOp
+    = BinOpTimes | BinOpDiv | BinOpAdd | BinOpSub | BinOpEq | BinOpNeq
+    | BinOpBindL | BinOpBindR | BinOpEllipses
+    | BinOpGt | BinOpGe | BinOpLt | BinOpLe
+    | BinOpSOr | BinOpSAnd | BinOpVOr | BinOpVAnd
+    | BinOpIn
     deriving  (Show, Read, Eq, Ord)
 
-data UnaryOp = UOpNegate | UOpNot
+data UnaryOp
+    = UOpNegate | UOpNot
     deriving (Show, Read, Eq, Ord)
 
 instance PPrint Expr
@@ -45,16 +47,19 @@ instance PPrint Expr
 -- projection specs
 -- ============================================================================
 
-data PSpec  = PSTable Bool [PSCol]    -- ^ build a new table by spec
-            | PSVectorName Identifier -- ^ extract vector by column name
-            | PSVectorNum Int         -- ^ extract vector by column number
-            deriving (Read, Show, Eq, Ord)
-data PSCol  = PSCNum Int              -- ^ column number
-            | PSCName Identifier      -- ^ column name
-            | PSCExp Identifier Expr  -- ^ ident=expr
-            | PSCStar                 -- ^ "star" for all columns
-            | PSCExpr Expr            -- ^ expression
-            deriving (Read, Show, Eq, Ord)
+data PSpec
+    = PSTable Bool [PSCol]    -- ^ build a new table by spec
+    | PSVectorName Identifier -- ^ extract vector by column name
+    | PSVectorNum Int         -- ^ extract vector by column number
+    deriving (Read, Show, Eq, Ord)
+
+data PSCol
+    = PSCNum Int              -- ^ column number
+    | PSCName Identifier      -- ^ column name
+    | PSCExp Identifier Expr  -- ^ ident=expr
+    | PSCStar                 -- ^ "star" for all columns
+    | PSCExpr Expr            -- ^ expression
+    deriving (Read, Show, Eq, Ord)
 
 -- ============================================================================
 -- join operators
