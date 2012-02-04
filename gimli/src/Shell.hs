4f9889781be211c3810f5d0c3531500c13f98b72
Tom Moertel <tom@moertel.com>
2012-02-04 15:34:50 -0500

    Bump version to 0.0.14

--- gimli/src/Shell.hs
+++ gimli/src/Shell.hs
@@ -64,7 +64,7 @@ welcome =
     , ""
     , Name.name
     , "Version " ++ Version.version
-    , "Copyright 2005-2007 Thomas G. Moertel."
+    , "Copyright 2005-2012 Thomas G. Moertel."
     , "All rights reserved worldwide."
     , "Licensed under the terms of the GNU General Public License, v. 3"
     , ""
